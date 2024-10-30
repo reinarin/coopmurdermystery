5309f420295d7374f3c02796e46a35b4c74561cb
reinarin <reinaa.li98@gmail.com>
2024-10-29 23:17:39 -0400

    Initial commit

--- R/check_suspect.R
+++ R/check_suspect.R
@@ -10,7 +10,7 @@
 
 check_suspect <- function(x) {
   if (x == "Jeremy Bowers") {
-    print("Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, try querying the interview transcript of the murderer to find the real villain behind this crime. If you feel especially confident in your R skills, try to complete this final step with no more than 2 (change me) queries. Use this same `check_suspect` function with your new suspect to check your answer.")
+    print("Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, try querying the interview transcript of the murderer to find the real villain behind this crime. If you feel especially confident in your R skills, try to complete this final step with no more than 20 lines of code. Use this same `check_suspect` function with your new suspect to check your answer.")
   } else if (x == "Miranda Priestly") {
     print("Congrats, you found the brains behind the murder! Everyone in R City hails you as the greatest R detective of all time. Time to break out the champagne!")
   } else {
