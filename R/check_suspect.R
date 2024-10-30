fe5a505620f01a5aedf9dd6863a4fab8fdd05afa
reinarin <reinaa.li98@gmail.com>
2024-10-29 21:55:29 -0400

    Initial commit

--- R/check_suspect.R
+++ R/check_suspect.R
@@ -2,14 +2,16 @@
 #'
 #' A function to check if the named suspect is the murderer/mastermind of the mystery"
 #'
-#' @param name A suspect's name.
-#' @returns A character vector.
+#' @param x A suspect's exact name.
+#' @returns A string.
 #' @examples
 #' check_suspect("Reina Li")
-check_suspect <- function(name) {
-  if (name == "Jeremy Bowers") {
+#' @export
+
+check_suspect <- function(x) {
+  if (x == "Jeremy Bowers") {
     print("Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, try querying the interview transcript of the murderer to find the real villain behind this crime. If you feel especially confident in your R skills, try to complete this final step with no more than 2 (change me) queries. Use this same `check_suspect` function with your new suspect to check your answer.")
-  } else if (name == "Miranda Priestly") {
+  } else if (x == "Miranda Priestly") {
     print("Congrats, you found the brains behind the murder! Everyone in R City hails you as the greatest R detective of all time. Time to break out the champagne!")
   } else {
     print("This person is not the murderer! Keep on investigating, sleuth!")
